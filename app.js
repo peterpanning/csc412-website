db8be03533bd4cdcfa00088c0f082962320cc824
peterpanning <peter2mutch@gmail.com>
2016-10-14 03:41:36 +0000

    Missed a pound sign

--- app.js
+++ app.js
@@ -1,3 +1,3 @@
 function changeNavBarColor() {
-  document.getElementByID("nav").style.backgroundColor=Math.floor(Math.random()*16777215).toString(16);
+  document.getElementById("nav").style.backgroundColor="#"+Math.floor(Math.random()*16777215).toString(16);
 }
