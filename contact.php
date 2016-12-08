3118fd44d9becc7fee1fb6978c47ad0b1a9e0383
peterpanning <peter2mutch@gmail.com>
2016-12-08 00:44:56 -0800

    Different tags for POST?

--- contact.php
+++ contact.php
@@ -50,15 +50,15 @@
             <form action="send_email.php" method="post">
               <div>
                   <label for="name">Name:</label>
-                  <input type="text" id="name" name="user_name" />
+                  <input type="text" id="name" name="name" />
               </div>
               <div>
                   <label for="mail">E-mail:</label>
-                  <input type="email" id="mail" name="user_email" />
+                  <input type="email" id="mail" name="mail" />
               </div>
               <div>
                   <label for="msg">Message:</label>
-                  <textarea id="msg" name="user_message"></textarea>
+                  <textarea id="msg" name="msg"></textarea>
               </div>
 
               <div class="button">
